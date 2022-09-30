857c8253f9570539e2d560a57defa3d61d4fd97a
Omnivers <Twersimor.stock@outlook.com>
2022-09-30 20:32:59 +0200

    Updates

---
+++ precache-manifest.ce33a983f5653274f33b505f28539fd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1936ee92fea69e7b5575cfe074942b0c",
+    "url": "/04-accordin/index.html"
+  },
+  {
+    "revision": "d8b47c7304cc38485965",
+    "url": "/04-accordin/static/css/main.718c89be.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/04-accordin/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/04-accordin/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b47c7304cc38485965",
+    "url": "/04-accordin/static/js/main.6312b6b1.chunk.js"
+  },
+  {
+    "revision": "0fc4f1f5b30c609515d6",
+    "url": "/04-accordin/static/js/runtime-main.079330ed.js"
+  }
+]);
